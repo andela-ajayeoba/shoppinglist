0178eb5a7ac1b8d709bcc7e2cbcc18227ccf0e5e
Jayeoba Ayodeji <dejijaye@yahoo.co.uk>
2014-09-08 16:55:19 +0100

    jquery complete,styling required

--- app/app.js
+++ app/app.js
@@ -1,6 +1,38 @@
-var complete = [];
-var uncomplete = [];
+var itemName;
+
+var shop = {
+	// add properties and actions.
+
+	addItems: function() {
+		itemName = $.trim($("#item-name").val());
+	},
+
+	validation: function() {
+
+		if (itemName === null || itemName === "" || itemName.length < 3 || itemName.length > 50 ) {
+			alert("enter a valid value")
+		}
+		else {
+			$("#uncomplete").append("<li>" + '<input type="checkbox" id="checker">' + itemName + '<button type="button" id="delete">' + "Delete" + "</button>" + "</li>");
+		}
+	}
+
+}
 
 $("#add").click(function() {
-	uncomplete.push($("#item-name").val());
+	shop.addItems();
+ 	shop.validation();
+ 	$("#item-name").val("");
+});
+
+$(document).on('click','#delete', function(){
+	$(this).parent().remove();
+})
+
+$("#uncomplete").on('change', 'input[type=checkbox]', function() {
+	$("#complete").append($(this).parent());
+});
+
+$("#complete").on('change', 'input[type=checkbox]', function() {
+	$("#uncomplete").append($(this).parent());
 });
